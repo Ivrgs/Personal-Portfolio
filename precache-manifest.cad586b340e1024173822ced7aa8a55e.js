322699e029527c9b47b91fd7bb1419d5423df238
Ivrgs <ivargasrodel@gmail.com>
2021-04-12 00:13:19 +0800

    Updates

---
+++ precache-manifest.cad586b340e1024173822ced7aa8a55e.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c53debba658a36cad5faa9b1a6826c60",
+    "url": "/Personal-Portfolio-React/index.html"
+  },
+  {
+    "revision": "3b7b71afe75167c8ce9b",
+    "url": "/Personal-Portfolio-React/static/css/2.a5f06b7f.chunk.css"
+  },
+  {
+    "revision": "6b16fa303c18bf4a751f",
+    "url": "/Personal-Portfolio-React/static/css/main.1359310a.chunk.css"
+  },
+  {
+    "revision": "3b7b71afe75167c8ce9b",
+    "url": "/Personal-Portfolio-React/static/js/2.d9fc9295.chunk.js"
+  },
+  {
+    "revision": "64f54ce285484345cd5e3bd96b120148",
+    "url": "/Personal-Portfolio-React/static/js/2.d9fc9295.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b16fa303c18bf4a751f",
+    "url": "/Personal-Portfolio-React/static/js/main.6776879b.chunk.js"
+  },
+  {
+    "revision": "f2475bd94d498294326c",
+    "url": "/Personal-Portfolio-React/static/js/runtime-main.e01bfa51.js"
+  },
+  {
+    "revision": "75784846dd148de3362e7131ed83e9fc",
+    "url": "/Personal-Portfolio-React/static/media/DIG_Transport-thumbnail.75784846.png"
+  },
+  {
+    "revision": "de6568cd318c0ab2d1c1f45261fe2761",
+    "url": "/Personal-Portfolio-React/static/media/Ecommerce-thumbnail.de6568cd.png"
+  },
+  {
+    "revision": "cb79b41836d97f07e9879286a6313405",
+    "url": "/Personal-Portfolio-React/static/media/Mobile-thumbnail.cb79b418.png"
+  },
+  {
+    "revision": "276df84c810a75607f1ca50c4afb677f",
+    "url": "/Personal-Portfolio-React/static/media/MyIcon.276df84c.png"
+  },
+  {
+    "revision": "fcd2a443b01ad250b052782861fdaebb",
+    "url": "/Personal-Portfolio-React/static/media/Responsive Device.fcd2a443.svg"
+  },
+  {
+    "revision": "6a2708a3574555a2d6b8540dbdf9778a",
+    "url": "/Personal-Portfolio-React/static/media/banner.6a2708a3.jpg"
+  },
+  {
+    "revision": "0e445768c8f6d5fee56ce3e9a2332f76",
+    "url": "/Personal-Portfolio-React/static/media/github.0e445768.svg"
+  },
+  {
+    "revision": "534505dc16de8681e95924c34436d998",
+    "url": "/Personal-Portfolio-React/static/media/gitlab.534505dc.svg"
+  },
+  {
+    "revision": "3fb871574f537a0253752d8025b7bf31",
+    "url": "/Personal-Portfolio-React/static/media/me.3fb87157.jpg"
+  },
+  {
+    "revision": "0bb9da434954672159b6611c81604a8d",
+    "url": "/Personal-Portfolio-React/static/media/responsive-I.0bb9da43.png"
+  },
+  {
+    "revision": "54496bfecff6913a8ae289c5186f7b58",
+    "url": "/Personal-Portfolio-React/static/media/storyBackground-01.54496bfe.svg"
+  }
+]);
