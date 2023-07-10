63999aa5e037704aeee27a21e293ec3ee5ac7579
Ivrgs <ivargasrodel@gmail.com>
2023-07-10 16:28:22 +0800

    opacity update

---
+++ build/precache-manifest.7e21702f36e3474af4693fbd2f1126c0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc5412f83eefb3d3739870aeeef390d4",
+    "url": "/Personal-Portfolio/index.html"
+  },
+  {
+    "revision": "3a3a7091fc5b09139081",
+    "url": "/Personal-Portfolio/static/css/2.637c884e.chunk.css"
+  },
+  {
+    "revision": "e123b7444edb34ad3fd7",
+    "url": "/Personal-Portfolio/static/css/main.0fb77066.chunk.css"
+  },
+  {
+    "revision": "3a3a7091fc5b09139081",
+    "url": "/Personal-Portfolio/static/js/2.5969a3f8.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/Personal-Portfolio/static/js/2.5969a3f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e123b7444edb34ad3fd7",
+    "url": "/Personal-Portfolio/static/js/main.fed9ede5.chunk.js"
+  },
+  {
+    "revision": "3fff9b48222206431347",
+    "url": "/Personal-Portfolio/static/js/runtime-main.4158b287.js"
+  },
+  {
+    "revision": "276df84c810a75607f1ca50c4afb677f",
+    "url": "/Personal-Portfolio/static/media/MyIcon.276df84c.png"
+  },
+  {
+    "revision": "9a32476b8bfaf65f12dcdfd95d4c4b32",
+    "url": "/Personal-Portfolio/static/media/aa-law.9a32476b.png"
+  },
+  {
+    "revision": "6a2708a3574555a2d6b8540dbdf9778a",
+    "url": "/Personal-Portfolio/static/media/banner.6a2708a3.jpg"
+  },
+  {
+    "revision": "81c6b7a30987e65ce122faeb0f0e737a",
+    "url": "/Personal-Portfolio/static/media/bearpaddle.81c6b7a3.png"
+  },
+  {
+    "revision": "6c61cb2b1c35c6e7408197c3244b57a8",
+    "url": "/Personal-Portfolio/static/media/codeigniter.6c61cb2b.png"
+  },
+  {
+    "revision": "a5bafd9fbe25f4e61f0392b6fbdb888c",
+    "url": "/Personal-Portfolio/static/media/github.a5bafd9f.svg"
+  },
+  {
+    "revision": "84997e8fa1c295903c22b920ce0e03dd",
+    "url": "/Personal-Portfolio/static/media/laravel.84997e8f.png"
+  },
+  {
+    "revision": "71db2df45b8bcb69a6081f5cceb55d67",
+    "url": "/Personal-Portfolio/static/media/lifeclinic.71db2df4.png"
+  },
+  {
+    "revision": "3507cb6e6014bf8f2fbc479d7944d152",
+    "url": "/Personal-Portfolio/static/media/me.3507cb6e.png"
+  },
+  {
+    "revision": "8e6470f1eb5bda53991bb080692b4016",
+    "url": "/Personal-Portfolio/static/media/rayito.8e6470f1.png"
+  },
+  {
+    "revision": "59aae50df59ecb09582f8c86f08f1717",
+    "url": "/Personal-Portfolio/static/media/react.59aae50d.png"
+  },
+  {
+    "revision": "0bb9da434954672159b6611c81604a8d",
+    "url": "/Personal-Portfolio/static/media/responsive-I.0bb9da43.png"
+  },
+  {
+    "revision": "87716ca442624c1299bc5b3fa21e5868",
+    "url": "/Personal-Portfolio/static/media/sprout.87716ca4.png"
+  },
+  {
+    "revision": "54496bfecff6913a8ae289c5186f7b58",
+    "url": "/Personal-Portfolio/static/media/storyBackground-01.54496bfe.svg"
+  },
+  {
+    "revision": "67ca932f969aa9ae06a76757da3fee54",
+    "url": "/Personal-Portfolio/static/media/utakpos.67ca932f.png"
+  },
+  {
+    "revision": "8606ee2f0f0a2823fccd14b60f439a3a",
+    "url": "/Personal-Portfolio/static/media/wordpress.8606ee2f.png"
+  }
+]);
