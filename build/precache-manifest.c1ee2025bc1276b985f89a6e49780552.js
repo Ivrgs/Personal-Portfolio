17495f223637950a6ff0f4427bf7258eade97a79
Ivrgs <ivargasrodel@gmail.com>
2023-10-18 15:55:31 +0800

    Package.json update

---
+++ build/precache-manifest.c1ee2025bc1276b985f89a6e49780552.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac9aef64b43c0b3ae8be1186bcf7595",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "8c2555288d448fc20c3f",
+    "url": "/personal-portfolio/static/css/main.9b5ecf52.chunk.css"
+  },
+  {
+    "revision": "f49363ff59b1b1c3285f",
+    "url": "/personal-portfolio/static/js/2.034beb57.chunk.js"
+  },
+  {
+    "revision": "e8ec38c230c1393ab3675e1979ec12cd",
+    "url": "/personal-portfolio/static/js/2.034beb57.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2555288d448fc20c3f",
+    "url": "/personal-portfolio/static/js/main.afd614f5.chunk.js"
+  },
+  {
+    "revision": "119b80b755d53bc48b41",
+    "url": "/personal-portfolio/static/js/runtime-main.b50c5e76.js"
+  },
+  {
+    "revision": "276df84c810a75607f1ca50c4afb677f",
+    "url": "/personal-portfolio/static/media/MyIcon.276df84c.png"
+  },
+  {
+    "revision": "6a2708a3574555a2d6b8540dbdf9778a",
+    "url": "/personal-portfolio/static/media/banner.6a2708a3.jpg"
+  },
+  {
+    "revision": "6c61cb2b1c35c6e7408197c3244b57a8",
+    "url": "/personal-portfolio/static/media/codeigniter.6c61cb2b.png"
+  },
+  {
+    "revision": "8d952bb7836c99cdca8433db908a3df6",
+    "url": "/personal-portfolio/static/media/game-commerce-design.8d952bb7.jpg"
+  },
+  {
+    "revision": "9760551bed8888f9af34839350a1a802",
+    "url": "/personal-portfolio/static/media/game-commerce-inventory.9760551b.jpg"
+  },
+  {
+    "revision": "9b335a30e5ce2d3ab5f5b176faff475a",
+    "url": "/personal-portfolio/static/media/game-commerce.9b335a30.jpg"
+  },
+  {
+    "revision": "b8741f4e73d27d21abd5a442dff0785b",
+    "url": "/personal-portfolio/static/media/github.b8741f4e.png"
+  },
+  {
+    "revision": "f406b58bf451afcd22399028f40d4c28",
+    "url": "/personal-portfolio/static/media/gmail.f406b58b.png"
+  },
+  {
+    "revision": "91c491b25e1b69f70e5b5827210d09ed",
+    "url": "/personal-portfolio/static/media/growth-rocket.91c491b2.jpg"
+  },
+  {
+    "revision": "d7e858efc4c8ea6afe2097c067806b53",
+    "url": "/personal-portfolio/static/media/html-css-js-jquery.d7e858ef.png"
+  },
+  {
+    "revision": "84997e8fa1c295903c22b920ce0e03dd",
+    "url": "/personal-portfolio/static/media/laravel.84997e8f.png"
+  },
+  {
+    "revision": "3507cb6e6014bf8f2fbc479d7944d152",
+    "url": "/personal-portfolio/static/media/me.3507cb6e.png"
+  },
+  {
+    "revision": "a68a4f6d7d964de4cac71b0cc2fc9f97",
+    "url": "/personal-portfolio/static/media/nodejs.a68a4f6d.png"
+  },
+  {
+    "revision": "86bf31510601ff16b00a976179673214",
+    "url": "/personal-portfolio/static/media/php.86bf3151.png"
+  },
+  {
+    "revision": "59aae50df59ecb09582f8c86f08f1717",
+    "url": "/personal-portfolio/static/media/react.59aae50d.png"
+  },
+  {
+    "revision": "6ae74755c154441751cb33c424b8a8cf",
+    "url": "/personal-portfolio/static/media/real-estate-design.6ae74755.jpg"
+  },
+  {
+    "revision": "c5b95f16a441f3bc1b7dbb7feb7d1728",
+    "url": "/personal-portfolio/static/media/rest-api.c5b95f16.png"
+  },
+  {
+    "revision": "7f5196242a7280cffc04bd5298895209",
+    "url": "/personal-portfolio/static/media/sass.7f519624.png"
+  },
+  {
+    "revision": "be50943a77f185801d32f8d605b1f921",
+    "url": "/personal-portfolio/static/media/sieranow-design.be50943a.jpg"
+  },
+  {
+    "revision": "54496bfecff6913a8ae289c5186f7b58",
+    "url": "/personal-portfolio/static/media/storyBackground-01.54496bfe.svg"
+  },
+  {
+    "revision": "f9765a9d5b4e367a6b33cacca066bc19",
+    "url": "/personal-portfolio/static/media/vector-outsourcing.f9765a9d.jpg"
+  },
+  {
+    "revision": "8606ee2f0f0a2823fccd14b60f439a3a",
+    "url": "/personal-portfolio/static/media/wordpress.8606ee2f.png"
+  }
+]);
